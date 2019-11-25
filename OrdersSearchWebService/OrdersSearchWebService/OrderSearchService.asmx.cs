0614ededbdf9f840214f36981567269282c79cb3
Kalyani <Kalyanikrishna09@gmail.com>
2019-11-25 10:29:22 -0600

    Initial check in for Order WebService

---
+++ OrdersSearchWebService/OrdersSearchWebService/OrderSearchService.asmx.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using OrdersSearchWebService.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace OrdersSearchWebService
+{
+    /// <summary>
+    /// Summary description for OrderSearchService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class OrderSearchService : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public List<Orders> SearchOrder(SearchOrderViewModel model, int page, int pageSize)
+        {
+            List<Orders> returnData = new List<Orders>();
+            bool modelValidation = false;
+            if (model.CompletionDte != null)
+            {
+                if (model.MSA > 0 && model.Status > 0)
+                {
+                    modelValidation = true;
+                }
+                else if (model.OrderId > 0)
+                {
+                    modelValidation = true;
+                }
+            }
+
+            if (modelValidation)
+            {
+                var data = GetJsondata();
+                if (data?.Any() ?? false)
+                {
+                    returnData = data
+                        .Where(x => (x.OrderId == model.OrderId || (x.MSA == model.MSA && x.Status == model.Status)) && x.CompletionDte.Date == model.CompletionDte.Date)?
+                        .Skip(page * pageSize).Take(pageSize).ToList();
+
+                }                
+            }
+            return returnData;
+        }
+
+        private List<Orders> GetJsondata()
+        {
+            using (StreamReader r = new StreamReader(Server.MapPath("~/JsonData/Orders.json")))
+            {
+                string json = r.ReadToEnd();
+                List<Orders> ro = JsonConvert.DeserializeObject<List<Orders>>(json);
+                return ro;
+            }
+        }
+    }
+}
---
+++ OrdersSearchWebService/OrdersSearchWebService/ViewModel/Orders.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrdersSearchWebService.ViewModel
+{
+    [Serializable]
+    public class Orders
+    {
+        public int OrderId { get; set; }
+        
+        public int ShipperId { get; set; }
+        
+        public int DriverId { get; set; }
+        
+        public DateTime CompletionDte { get; set; }
+        
+        public int Status { get; set; }
+        
+        public string Code { get; set; }
+        
+        public int MSA { get; set; }
+        
+        public float Duration { get; set; }
+        
+        public int OfferType { get; set; }
+    }
+}
