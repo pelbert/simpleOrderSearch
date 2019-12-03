57aaa9db0b5105d4211de9674a51cc25d7b57101
Sangeetha Vasudevan <sangeetha_vaasu@yahoo.com>
2019-12-03 01:33:27 -0600

    Web service

---
+++ OrderSearchAPI/OrderSearchAPI/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace OrderSearchAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ OrderSearchAPI/OrderSearchAPI/Models/OrderInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrderSearchAPI.Models
+{
+
+    public class OrderInfo
+    {
+        public class OrderData
+        {
+            public int OrderID { get; set; }
+            public int ShipperID { get; set; }
+            public int DriverID { get; set; }
+            public DateTime CompletionDte { get; set; }
+            public int Status { get; set; }
+            public string Code { get; set; }
+            public int MSA { get; set; }
+            public string Duration { get; set; }
+            public int OfferType { get; set; }
+        }
+
+    }
+}
