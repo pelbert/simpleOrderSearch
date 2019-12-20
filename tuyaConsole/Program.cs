6167457753d1f6ef60cb42077e3c2b8721135140
Jasmit Tarang <jaytarang92@gmail.com>
2019-12-20 03:28:38 -0600

    TUYA-01: Committing initial files.

---
+++ tuyaConsole/Program.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.AspNetCore.WebUtilities;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace tuyaConsole
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var exitApplicaion = false;
+            var tuyaHttp = new TuyaHttpClient();
+            do
+            {
+                MainMenu();
+                var keyEntered = Console.ReadKey();
+                switch (keyEntered.Key.ToString())
+                {
+                    case "D1":
+                        Console.WriteLine("\nEnter OrderID\n");
+                        var orderId = Console.ReadLine();
+                        Console.WriteLine(JObject.Parse(tuyaHttp.GetRequestAsync($"/api/orders/{orderId}").GetAwaiter().GetResult()));
+                        break;
+                    case "D2":
+                        var queryParamsDict = new Dictionary<string, string>();
+                        Console.WriteLine("\nEnter MSA\n");
+                        queryParamsDict.Add("msa", Console.ReadLine());
+                        Console.WriteLine("\nEnter Status\n");
+                        queryParamsDict.Add("status", Console.ReadLine());
+                        Console.WriteLine("\nEnter Completion Date:\n");
+                        queryParamsDict.Add("completionDate", Console.ReadLine());
+                        Console.WriteLine(JObject.Parse(tuyaHttp.GetRequestAsync($"/api/orders/query", queryParamsDict).GetAwaiter().GetResult()));
+                        break;
+                    case "D3":
+                        exitApplicaion = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid selection and try again!");
+                        break;
+                }
+            } while (!exitApplicaion);
+            Console.WriteLine("\nThanks for using the tool. Closing Session.");
+        }
+
+        private static void MainMenu()
+        {
+            Console.WriteLine("\n\n============== Tuya API Tool ================\n");
+            Console.WriteLine("Make a selection to query orders:");
+            Console.WriteLine("\t1. Find order by `OrderID`\n\t2. Find order by `MSA, Status, CompletionDate`\n\t3. Exit\n");
+        }
+    }
+
+    public class TuyaHttpClient
+    {
+        private readonly string ApiScheme = "https";
+        private readonly string ApiHost = "127.0.0.1";
+        private readonly int ApiPort = 5001; 
+
+        private string CreateUri(string path = null, Dictionary<string, string> queryParams = null)
+        {
+            queryParams = queryParams ?? new Dictionary<string, string>();
+            var queryString = QueryHelpers.AddQueryString(path, queryParams);
+            return $"{ApiScheme}://{ApiHost}:{ApiPort}{queryString}";
+        }
+
+        public async Task<string> GetRequestAsync(string path = null, Dictionary<string, string> queryParams = null)
+        {
+            var apiUri = CreateUri(path, queryParams);
+            using (var httpClientHandler = new HttpClientHandler())
+            {
+                httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+                using var client = new HttpClient(httpClientHandler);
+                var response = await client.GetAsync(apiUri);
+                switch (response.StatusCode)
+                {
+                    case HttpStatusCode.OK:
+                        return await response.Content.ReadAsStringAsync();
+                    case HttpStatusCode.NotFound:
+                        return new JObject { { "error", "Order does not exist!" } }.ToString();
+                    default:
+                        throw new HttpRequestException($"Status Code: {response.StatusCode}\n" +
+                                                       $"Response {response.Content.ReadAsStringAsync().Result}");
+                };
+
+            }
+        }
+    }
+}
---
+++ tuyaServer/Controllers/OrdersController.cs
@@ -0,0 +1,133 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using tuya.Models;
+using tuyaServer.Pagination;
+
+namespace tuya.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrdersController : ControllerBase
+    {
+        private readonly OrderContext _context;
+
+        public OrdersController(OrderContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Orders
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Order>>> GetOrders()
+        {
+            return await _context.Orders.ToListAsync();
+        }
+
+        // GET: api/Orders/query
+        /*
+         * Query Params:
+         *     int      msa
+         *     int      status
+         *     string   completionDate
+         */
+        [HttpGet("query")]
+        public PagedResult<Order> QueryOrders( [FromQuery] [Required] int msa, [Required]int status, [Required]string completionDate, int pageNo = 1, int pageSize = 10)
+        {
+            int skip = (pageNo - 1) * pageSize;
+
+            var orders = _context.Orders.ToList();
+            var matchingOrders = orders.Where(o =>
+                (o.MSA == msa && o.Status == status) && o.CompletionDate == completionDate)
+                .OrderBy(o => o.OrderID)
+                .Skip(skip)
+                .Take(pageSize)
+                .ToList();
+
+            return new PagedResult<Order>(matchingOrders, pageNo, pageSize, matchingOrders.Count);
+
+        }
+
+        // GET: api/Orders/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Order>> GetOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            return order;
+        }
+
+        // PUT: api/Orders/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutOrder(int id, Order order)
+        {
+            if (id != order.OrderID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(order).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!OrderExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Orders
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Order>> PostOrder(Order order)
+        {
+            _context.Orders.Add(order);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetOrder", new { id = order.OrderID }, order);
+        }
+
+        // DELETE: api/Orders/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Order>> DeleteOrder(int id)
+        {
+            var order = await _context.Orders.FindAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+
+            return order;
+        }
+
+        private bool OrderExists(int id)
+        {
+            return _context.Orders.Any(e => e.OrderID == id);
+        }
+    }
+}
